25551b06720a9c9ef445136476aab9157e71e6f1
Md Reaz Morshed <nehal.sunderland@gmail.com>
2020-10-15 19:27:10 +0200

    testing github

--- RGL_G33_Java_Code/src/Magic.java
+++ RGL_G33_Java_Code/src/Magic.java
@@ -1,4 +1,4 @@
 public class Magic {
 
-    private int mana;
+    private int mana ;
 }
--- RGL_G33_Java_Code/src/map.java
+++ RGL_G33_Java_Code/src/map.java
@@ -1,4 +1,7 @@
 public class map {
     private int x,y;
 
+    public static void main(String[] args) {
+        String.format("I am trying to merge into my branch Map", );
+    } 
 }
