68d6baa8648de1be2605596b4538095b6134308e
Md Reaz Morshed <nehal.sunderland@gmail.com>
2020-10-17 15:35:33 +0200

    1changed

--- RGL_G33_Java_Code/src/map.java
+++ RGL_G33_Java_Code/src/map.java
@@ -1,5 +1,31 @@
+import javax.swing.*;
+
 public class map {
-    private int x;
+    private JFrame frame;
+    private String title;
+    private int height;
+    private int weidth;
+    private JFrame map;
+
+
+    public map(String title, int height, int weidth) {
+        this.title = title;
+        this.height = height;
+        this.weidth = weidth;
+
+        gameDisplay();
+    }
+
+    private void gameDisplay() {
+
+        frame= new JFrame(title);
+        frame.setSize(weidth,height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);// user cant drag
+        frame.setLocationRelativeTo(null);// screen will be in center of the screen.
+        frame.setVisible(true);
+
+    }
 
 
 }
