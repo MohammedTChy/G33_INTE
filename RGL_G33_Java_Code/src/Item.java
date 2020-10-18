0bd63e05fe8ffc4fbb19ed28b61e40b638fe61b0
ElderGX <62905235+ElderGX@users.noreply.github.com>
2020-10-18 20:41:34 +0200

    Re-drawn idea of potion, still W.I.P

--- RGL_G33_Java_Code/src/Item.java
+++ RGL_G33_Java_Code/src/Item.java
@@ -1,5 +1,9 @@
 public class Item {
 
+    //W.I.P idea
+    /*public enum PotionType {
+        Health_Potion, Mana_Potion
+    }*/
 
     private final String name;
     private final String potionType;
@@ -24,6 +28,7 @@ public class Item {
     }
 
     //Potion interaktion, inte nöjd men en början, har ej fixat mana då den inte finns med
+    //Idéen är att den kollar vilken typ av potion det är, tar den potion och adderar upp, kollar ej limit, då det finns inte för tilfället med att lägga det.
     public void usePotion(Player player, Item item) {
         if (item.potionType.equals("Health Potion")) {
             int hP = player.getHitPoints();
