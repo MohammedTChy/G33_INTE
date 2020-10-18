4506e7c821d1c2b1cb3c141a3d1c29c8531d8dae
Max <bluetiger.thelin@gmail.com>
2020-10-18 21:07:19 +0200

    sketch code

--- RGL_G33_Java_Code/src/Combat.java
+++ RGL_G33_Java_Code/src/Combat.java
@@ -1,9 +1,36 @@
 
 public class Combat {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private player character;
+	private man monster;
+	private boolean playerTurn;
+	private boolean activeCombat;
+	
+	public Combat(player player, man monster) {
+		this.monster = monster;
+		character = player;
+		playerTurn = true;
+		activeCombat = true;
 	}
-
+	
+	public void turn(){
+	
+		if(character.getHealth()<0) {
+			activeCombat=false;
+		}
+		else if(monster.getHealth()<0) {
+			activeCombat=false;
+		}
+		
+		else if (playerTurn) {
+			PlayerTurn(character);
+		}
+		
+		else {
+			MonsterTurn(monster);
+		}
+	}
+	
+	
+	
 }
---
+++ RGL_G33_Java_Code/src/PlayerTurn.java
@@ -0,0 +1,21 @@
+
+public class PlayerTurn {
+
+	private man player;
+	
+	
+	
+	public int attack() {
+		return 5;
+	}
+	
+	public int powerAttack() {
+		return 10;
+	}
+	
+	public int spell() {
+		return 5;
+	}
+	//??? oh my gawd
+
+}
