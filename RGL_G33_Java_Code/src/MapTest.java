54ce062ea2fcff436ce678a62a00d38e89c3e0c9
Md Reaz Morshed <nehal.sunderland@gmail.com>
2020-10-18 12:09:22 +0200

    1changed

--- RGL_G33_Java_Code/src/MapTest.java
+++ RGL_G33_Java_Code/src/MapTest.java
@@ -11,5 +11,11 @@ class MapTest {
         Map map=new Map(10,15,tile);
         assertEquals(map.getWidth(),10);
     }
+    @Test// just wrote this test.
+    void TestBreadth(){
+        Tile tile = new Tile();
+        Map map=new Map(12,14,tile);
+        assertEquals(map.getBreadth(),14);
+    }
 
 }
