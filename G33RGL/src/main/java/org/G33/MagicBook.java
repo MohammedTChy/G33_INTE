a195497ea9b0207bb8accaa100383fd8b5a9b7d3
ElderGX <62905235+ElderGX@users.noreply.github.com>
2020-10-23 10:57:54 +0200

    Maven

--- G33RGL/src/main/java/org/G33/MagicBook.java
+++ G33RGL/src/main/java/org/G33/MagicBook.java
@@ -1,5 +1,6 @@
+package org.G33;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MagicBook {
@@ -8,11 +9,9 @@ public class MagicBook {
     //added static Magic before hand is possible, did not do it mainly because of not needed to yet
     private List<Magic> magicBook = new ArrayList<>();
 
-    //Call on this to get the entire list of spells in the book.
-    /*public List<Magic> getMagicBook() {
+    public List<Magic> getMagicBook() {
         return magicBook;
-    }*/
-
+    }
 
     public void addMagicBook(String name, int manaCost, int damage) {
         magicBook.add(new Magic(name, manaCost, damage));
---
+++ G33RGL/src/main/java/org/G33/Man.java
@@ -0,0 +1,19 @@
+package org.G33;
+
+public class Man extends Creature {
+
+    private int mana = 0;
+    public Man(int hitPoints, int attackPower, int defensePower) {
+        super(hitPoints, attackPower, defensePower);
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    @Override
+    public String toString() {
+        return "HP: " + getHitPoints() +
+                ", Attack: " + getAttackPower() + ", Defense: " + getDefensePower();
+    }
+}
