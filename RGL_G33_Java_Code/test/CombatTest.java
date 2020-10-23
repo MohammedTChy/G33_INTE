9eb4868e3580f5be5b3d8249623ee02a20bb2776
Max <bluetiger.thelin@gmail.com>
2020-10-23 12:06:01 +0200

    update

--- RGL_G33_Java_Code/test/CombatTest.java
+++ RGL_G33_Java_Code/test/CombatTest.java
@@ -17,19 +17,5 @@ class CombatTest {
 
 
 
-/* Krav lista:
- * 
- * monsters (man) loseHealth() metod
- * monsters har defensePower
- * monsters har attackPower
- * dessa har getDefensePower()
- * och getAttackPower() metoder, returnerar såklart 
- * int värden
- * 
- */
-
-
-
-
 
 
