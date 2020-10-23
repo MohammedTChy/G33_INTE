458a6ef07a9042732cd10309f66f6f86eeb53b03
Md Reaz Morshed <nehal.sunderland@gmail.com>
2020-10-23 21:44:29 +0200

    ChangedNowFromManToCreature

--- RGL_G33_Java_Code/src/Creature.java
+++ RGL_G33_Java_Code/src/Creature.java
@@ -3,7 +3,7 @@ public abstract class Creature {
     private int hitPoints;
     private int attackPower;
     private int defensePower;
-
+    private Position mansPosition;//plz dont chng it change this method, it was created to interact with map.
     public Creature(int hitPoints, int attackPower, int defensePower) {
         checkHitPointsNotBelowZero(hitPoints);
         this.hitPoints = hitPoints;
@@ -24,7 +24,17 @@ public abstract class Creature {
         if (hitPoints < 0) {
             throw new IllegalArgumentException("HP less than 0");
         }
+
     }
+
+    public Position getMansPosition() {  // plz dont chng it change this method, it was created to interact with map.
+        return mansPosition;
+    }
+
+    public void setMansPosition(Position mansPosition) {//plz dont chng it change this method, it was created to interact with map.
+        this.mansPosition = mansPosition;
+    }
+
 
     public int getAttackPower() {
         return attackPower;
--- RGL_G33_Java_Code/src/Man.java
+++ RGL_G33_Java_Code/src/Man.java
@@ -1,7 +1,6 @@
 public class Man extends Creature {
 
     private int mana = 0;
-    private Position mansPosition;//plz dont chng it change this method, it was created to interact with map.
     public Man(int hitPoints, int attackPower, int defensePower) {
         super(hitPoints, attackPower, defensePower);
     }
@@ -10,13 +9,6 @@ public class Man extends Creature {
         return mana;
     }
 
-    public Position getMansPosition() {  // plz dont chng it change this method, it was created to interact with map.
-        return mansPosition;
-    }
-
-    public void setMansPosition(Position mansPosition) {//plz dont chng it change this method, it was created to interact with map.
-        this.mansPosition = mansPosition;
-    }
 
     @Override
     public String toString() {
