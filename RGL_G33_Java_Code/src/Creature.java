f26f4d96e3faddb93aac367a43ecd9cd05201e2c
Md Reaz Morshed <nehal.sunderland@gmail.com>
2020-10-25 11:44:22 +0100

    newBranch

--- RGL_G33_Java_Code/src/Creature.java
+++ RGL_G33_Java_Code/src/Creature.java
@@ -1,16 +1,14 @@
 public abstract class Creature {
-
     private int hitPoints;
     private int attackPower;
     private int defensePower;
-
+    private Position mansPosition;//************************************************
     public Creature(int hitPoints, int attackPower, int defensePower) {
         checkHitPointsNotBelowZero(hitPoints);
         this.hitPoints = hitPoints;
         this.attackPower = attackPower;
         this.defensePower = defensePower;
     }
-
     public int getHitPoints() {
         return hitPoints;
     }
@@ -25,11 +23,16 @@ public abstract class Creature {
             throw new IllegalArgumentException("HP less than 0");
         }
     }
+    public Position getMansPosition() {  // plz dont chng it change this method, it was created to interact with map.
+        return mansPosition;
+    }//**************************
+    public void setMansPosition(Position mansPosition) {//plz dont chng it change this method, it was created to interact with map.
+        this.mansPosition = mansPosition;
+    }//**************
 
     public int getAttackPower() {
         return attackPower;
     }
-
     public int getDefensePower() {
         return defensePower;
     }
